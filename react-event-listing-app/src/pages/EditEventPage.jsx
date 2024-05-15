1cb431d8a44d4497f6b34629691a97657abd44cf
Stephanie Micah Cahutay <stephaniemicah.cahutay@cit.edu>
2024-05-15 23:38:57 +0800

    Add id in edit event page

--- react-event-listing-app/src/pages/EditEventPage.jsx
+++ react-event-listing-app/src/pages/EditEventPage.jsx
@@ -19,10 +19,11 @@ const EditEventPage = ({ updateEventSubmit }) => {
     const navigate = useNavigate();
     const { id } = useParams();
 
-    const submitForm = (e) => {
+    const submitForm = async (e) => {
         e.preventDefault();
 
         const updatedEvent = {
+            id,
             title,
             type,
             location,
@@ -38,11 +39,14 @@ const EditEventPage = ({ updateEventSubmit }) => {
             },
         };
 
-        updateEventSubmit(updatedEvent);
-
-        toast.success('Event Updated Successfully');
-
-        return navigate(`/events/${id}`);
+        try {
+            await updateEventSubmit(updatedEvent);
+            toast.success('Event Updated Successfully');
+            navigate(`/events/${id}`);
+        } catch (error) {
+            toast.error('Failed to update event. Please try again.');
+            console.error("Error updating event:", error);
+        }
     };
 
     return (
@@ -53,20 +57,8 @@ const EditEventPage = ({ updateEventSubmit }) => {
                         <h2 className='text-3xl text-center font-semibold mb-6'>Update Event</h2>
 
                         <div className='mb-4'>
-                            <label
-                                htmlFor='type'
-                                className='block text-gray-700 font-bold mb-2'
-                            >
-                                Event Type
-                            </label>
-                            <select
-                                id='type'
-                                name='type'
-                                className='border rounded w-full py-2 px-3'
-                                required
-                                value={type}
-                                onChange={(e) => setType(e.target.value)}
-                            >
+                            <label htmlFor='type' className='block text-gray-700 font-bold mb-2'>Event Type</label>
+                            <select id='type' name='type' className='border rounded w-full py-2 px-3' required value={type} onChange={(e) => setType(e.target.value)}>
                                 <option value='Arts'>Arts</option>
                                 <option value='Cultural'>Cultural</option>
                                 <option value='Technology'>Technology</option>
@@ -81,182 +73,59 @@ const EditEventPage = ({ updateEventSubmit }) => {
                         </div>
 
                         <div className='mb-4'>
-                            <label className='block text-gray-700 font-bold mb-2'>
-                                Event Listing Name
-                            </label>
-                            <input
-                                type='text'
-                                id='title'
-                                name='title'
-                                className='border rounded w-full py-2 px-3 mb-2'
-                                placeholder='eg. Creative Workshops Series'
-                                required
-                                value={title}
-                                onChange={(e) => setTitle(e.target.value)}
-                            />
-                        </div>
-                        <div className='mb-4'>
-                            <label
-                                htmlFor='description'
-                                className='block text-gray-700 font-bold mb-2'
-                            >
-                                Description
-                            </label>
-                            <textarea
-                                id='description'
-                                name='description'
-                                className='border rounded w-full py-2 px-3'
-                                rows='4'
-                                placeholder='Add any event duties, expectations, requirements, etc'
-                                value={description}
-                                onChange={(e) => setDescription(e.target.value)}
-                            ></textarea>
+                            <label className='block text-gray-700 font-bold mb-2'>Event Listing Name</label>
+                            <input type='text' id='title' name='title' className='border rounded w-full py-2 px-3 mb-2' placeholder='eg. Creative Workshops Series' required value={title} onChange={(e) => setTitle(e.target.value)} />
                         </div>
 
                         <div className='mb-4'>
-                            <label
-                                htmlFor='date'
-                                className='block text-gray-700 font-bold mb-2'
-                            >
-                                Event Date and Time
-                            </label>
-                            <input
-                                type='datetime-local'
-                                id='date'
-                                name='date'
-                                className='border rounded w-full py-2 px-3'
-                                required
-                                value={date}
-                                onChange={(e) => setDate(e.target.value)}
-                            />
-                        </div>
-                        <div className='mb-4'>
-                            <label htmlFor='ticketPrice' className='block text-gray-700 font-bold mb-2'>
-                                Ticket Price
-                            </label>
-                            <input
-                                type='text'
-                                id='ticketPrice'
-                                name='ticketPrice'
-                                className='border rounded w-full py-2 px-3'
-                                placeholder='Enter ticket price'
-                                value={ticketPrice}
-                                onChange={(e) => setTicketPrice(e.target.value)}
-                                required
-                            />
+                            <label htmlFor='description' className='block text-gray-700 font-bold mb-2'>Description</label>
+                            <textarea id='description' name='description' className='border rounded w-full py-2 px-3' rows='4' placeholder='Add any event duties, expectations, requirements, etc' value={description} onChange={(e) => setDescription(e.target.value)}></textarea>
                         </div>
 
                         <div className='mb-4'>
-                            <label htmlFor='capacity' className='block text-gray-700 font-bold mb-2'>
-                                Capacity
-                            </label>
-                            <input
-                                type='text'
-                                id='capacity'
-                                name='capacity'
-                                className='border rounded w-full py-2 px-3'
-                                placeholder='Enter event capacity'
-                                value={capacity}
-                                onChange={(e) => setCapacity(e.target.value)}
-                                required
-                            />
+                            <label htmlFor='date' className='block text-gray-700 font-bold mb-2'>Event Date and Time</label>
+                            <input type='datetime-local' id='date' name='date' className='border rounded w-full py-2 px-3' required value={date} onChange={(e) => setDate(e.target.value)} />
                         </div>
 
                         <div className='mb-4'>
-                            <label className='block text-gray-700 font-bold mb-2'>
-                                Location
-                            </label>
-                            <input
-                                type='text'
-                                id='location'
-                                name='location'
-                                className='border rounded w-full py-2 px-3 mb-2'
-                                placeholder='Organizer Location'
-                                required
-                                value={location}
-                                onChange={(e) => setLocation(e.target.value)}
-                            />
+                            <label htmlFor='ticketPrice' className='block text-gray-700 font-bold mb-2'>Ticket Price</label>
+                            <input type='text' id='ticketPrice' name='ticketPrice' className='border rounded w-full py-2 px-3' placeholder='Enter ticket price' value={ticketPrice} onChange={(e) => setTicketPrice(e.target.value)} required />
+                        </div>
+
+                        <div className='mb-4'>
+                            <label htmlFor='capacity' className='block text-gray-700 font-bold mb-2'>Capacity</label>
+                            <input type='text' id='capacity' name='capacity' className='border rounded w-full py-2 px-3' placeholder='Enter event capacity' value={capacity} onChange={(e) => setCapacity(e.target.value)} required />
+                        </div>
+
+                        <div className='mb-4'>
+                            <label className='block text-gray-700 font-bold mb-2'>Location</label>
+                            <input type='text' id='location' name='location' className='border rounded w-full py-2 px-3 mb-2' placeholder='Organizer Location' required value={location} onChange={(e) => setLocation(e.target.value)} />
                         </div>
 
                         <h3 className='text-2xl mb-5'>Organizer Info</h3>
 
                         <div className='mb-4'>
-                            <label
-                                htmlFor='organizer'
-                                className='block text-gray-700 font-bold mb-2'
-                            >
-                                Organizer Name
-                            </label>
-                            <input
-                                type='text'
-                                id='organizer'
-                                name='organizer'
-                                className='border rounded w-full py-2 px-3'
-                                placeholder='Organizer Name'
-                                value={organizerName}
-                                onChange={(e) => setOrganizerName(e.target.value)}
-                            />
+                            <label htmlFor='organizer' className='block text-gray-700 font-bold mb-2'>Organizer Name</label>
+                            <input type='text' id='organizer' name='organizer' className='border rounded w-full py-2 px-3' placeholder='Organizer Name' value={organizerName} onChange={(e) => setOrganizerName(e.target.value)} />
                         </div>
 
                         <div className='mb-4'>
-                            <label
-                                htmlFor='organizer_description'
-                                className='block text-gray-700 font-bold mb-2'
-                            >
-                                Organizer Description
-                            </label>
-                            <textarea
-                                id='organizer_description'
-                                name='organizer_description'
-                                className='border rounded w-full py-2 px-3'
-                                rows='4'
-                                placeholder='What does your organizer do?'
-                                value={organizerDescription}
-                                onChange={(e) => setOrganizerDescription(e.target.value)}
-                            ></textarea>
+                            <label htmlFor='organizer_description' className='block text-gray-700 font-bold mb-2'>Organizer Description</label>
+                            <textarea id='organizer_description' name='organizer_description' className='border rounded w-full py-2 px-3' rows='4' placeholder='What does your organizer do?' value={organizerDescription} onChange={(e) => setOrganizerDescription(e.target.value)}></textarea>
                         </div>
 
                         <div className='mb-4'>
-                            <label
-                                htmlFor='contact_email'
-                                className='block text-gray-700 font-bold mb-2'
-                            >
-                                Contact Email
-                            </label>
-                            <input
-                                type='email'
-                                id='contact_email'
-                                name='contact_email'
-                                className='border rounded w-full py-2 px-3'
-                                placeholder='Email address for organizers'
-                                required
-                                value={contactEmail}
-                                onChange={(e) => setContactEmail(e.target.value)}
-                            />
+                            <label htmlFor='contact_email' className='block text-gray-700 font-bold mb-2'>Contact Email</label>
+                            <input type='email' id='contact_email' name='contact_email' className='border rounded w-full py-2 px-3' placeholder='Email address for organizers' required value={contactEmail} onChange={(e) => setContactEmail(e.target.value)} />
                         </div>
+
                         <div className='mb-4'>
-                            <label
-                                htmlFor='contact_phone'
-                                className='block text-gray-700 font-bold mb-2'
-                            >
-                                Contact Phone
-                            </label>
-                            <input
-                                type='tel'
-                                id='contact_phone'
-                                name='contact_phone'
-                                className='border rounded w-full py-2 px-3'
-                                placeholder='Optional phone for organizers'
-                                value={contactPhone}
-                                onChange={(e) => setContactPhone(e.target.value)}
-                            />
+                            <label htmlFor='contact_phone' className='block text-gray-700 font-bold mb-2'>Contact Phone</label>
+                            <input type='tel' id='contact_phone' name='contact_phone' className='border rounded w-full py-2 px-3' placeholder='Optional phone for organizers' value={contactPhone} onChange={(e) => setContactPhone(e.target.value)} />
                         </div>
 
                         <div>
-                            <button
-                                className='bg-[#EA7E5D hover:bg-[#FF6347]  text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline'
-                                type='submit'
-                            >
+                            <button className='bg-[#EA7E5D] hover:bg-[#FF6347] text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline' type='submit'>
                                 Update Event
                             </button>
                         </div>
@@ -267,4 +136,4 @@ const EditEventPage = ({ updateEventSubmit }) => {
     );
 };
 
-export default EditEventPage
+export default EditEventPage;
